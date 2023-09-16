dfe9f860c682d3090b82f70eb267f07bc8086067
DELL <hassanyasser1313@gmail.com>
2023-09-16 11:58:36 +0300

    hello

---
+++ lib/core/hello/hello.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class hhh extends StatelessWidget {
+  const hhh({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('data');
+  }
+}
