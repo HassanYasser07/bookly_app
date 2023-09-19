c316b8478a6106dc456305e9781442ac320a0d1e
DELL <hassanyasser1313@gmail.com>
2023-09-19 16:00:49 +0300

    create book_details_view.dart

---
+++ lib/features/home/presentation/views/book_details_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class BoolDetailsView extends StatelessWidget {
+  const BoolDetailsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
