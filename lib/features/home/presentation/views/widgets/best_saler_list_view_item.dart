c53752025c52bbc92eab36b73fc07575abcfa255
DELL <hassanyasser1313@gmail.com>
2023-09-19 14:32:12 +0300

    update best seller item done

--- lib/features/home/presentation/views/widgets/best_saler_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_saler_list_view_item.dart
@@ -1,4 +1,3 @@
-import 'package:bookly_app/features/home/presentation/views/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 import '../../../../../core/utils/assets_image.dart';
 import '../../../../../core/utils/style.dart';
