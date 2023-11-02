e1449bcf135910cb1f7f79ea71ae4045fbffc491
DELL <hassanyasser1313@gmail.com>
2023-11-02 13:07:08 +0200

    create featured books model state

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -27,8 +27,21 @@ class HomeRepoImpl implements HomeRepo{
     }}
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() {
-    // TODO: implement fetchFeatureBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() async{
+    try{
+      var data=await apiServices.git(endPoint: 'volumes? Filtering=free-ebooks&Sorting=newest &q=subject:Programming');
+      List<BookModel> books=[];
+      for(var item in data['items']){
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+
+    }on Exception catch(e){
+      if(e is DioException){
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+
   }
 }
