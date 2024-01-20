ab11f2ff67b93fb802f72a6d775aede5474cec76
DELL <hassanyasser1313@gmail.com>
2024-01-20 18:41:27 +0200

    using cached network image

--- lib/features/home/presentation/views/widgets/custombook_item.dart
+++ lib/features/home/presentation/views/widgets/custombook_item.dart
@@ -1,19 +1,20 @@
- import 'package:flutter/material.dart';
+ import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
 
 class CustomBookItem extends StatelessWidget {
   const CustomBookItem({Key? key, required this.imageUrl, }) : super(key: key);
  final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.5/4,
-      child: Container(
-        decoration:  BoxDecoration(
-          image: DecorationImage(
-              fit: BoxFit.fill,
-              image:NetworkImage(imageUrl!)
-          ),
-        ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.5/4,
+        child:CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) => Icon(Icons.error),
+        )
       ),
     );
   }
