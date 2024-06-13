aaa5baf9712a3d88853712a32b4a8fe9ccd69263
HassanYasser07 <hassanyasser1313@gmail.com>
2024-06-13 03:15:57 +0300

    create failure class Create Featured Books Cubit State

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,35 +1,78 @@
+
 import 'package:bookly_app/core/errrs/failure.dart';
 import 'package:bookly_app/core/utils/api_services.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
-
-class HomeRepoImpl implements HomeRepo{
- final ApiServices apiServices;
+class HomeRepoImpl implements HomeRepo {
+  final ApiServices apiServices;
 
   HomeRepoImpl(this.apiServices);
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async{
-    try{
-      var data=await  apiServices.get(endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:Programming');
-      List<BookModel> books=[];
-
-          for(var item in data['items'] ){
-            books.add(BookModel.fromJson(item));
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiServices.get(
+          endPoint:
+          'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        try {
+          books.add(BookModel.fromJson(item));
+        } catch (e) {
+          books.add(BookModel.fromJson(item));
+        }
       }
-      return Right(books);
 
-    }  catch(e){
-      return Left(Failure());
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
+  }
 
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async{
+    try {
+      var data = await apiServices.get(
+          endPoint:
+          'volumes?Filtering=free-ebooks&q=subject:Programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        try {
+          books.add(BookModel.fromJson(item));
+        } catch (e) {
+          books.add(BookModel.fromJson(item));
+        }
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
     }
 
   }
 
-  @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
-  }
+
+
+
+
 }
