917e975a7e5ff5c73f17d8e8e8250e8d4d3c3f2b
HassanYasser07 <hassanyasser1313@gmail.com>
2024-06-23 22:55:15 +0300

    newest Books cubit done

--- lib/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
@@ -2,7 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
-import '../../data/models/book_model/book_model.dart';
+import '../../../data/models/book_model/book_model.dart';
 
 part 'featured_books_state.dart';
 
